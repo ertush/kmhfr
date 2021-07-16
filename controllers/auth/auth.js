1c704b928199e0aba58d07baf3fb849f92be47cb
Benzer Bett <benzerbett@gmail.com>
2021-07-17 00:35:35 +0300

    login wip

--- controllers/auth/auth.js
+++ controllers/auth/auth.js
@@ -4,21 +4,23 @@ const FormData = require('form-data');
 const Cookies = require('cookies')
 const cookieCutter = require('cookie-cutter')
 
-const getToken = (refresh_token) => {
+const getToken = (req, res, refresh_token, creds) => {
+    const isServer = !!req
+    const isBrowser = !req
     console.log('<<<<<<<<<<<<<<<<<<<<<<<<<<<<<')
     const bod = {} //new FormData();
     if (refresh_token && refresh_token.length > 0) {
         console.log('Refreshing token...')
-        bod.grant_type = "refresh_token" // bod.append("grant_type", "refresh_token");
-        bod.refresh_token = refresh_token // bod.append("refresh_token", refresh_token);
+        bod.grant_type = "refresh_token"
+        bod.refresh_token = refresh_token
     } else {
         console.log('Getting new token...')
-        bod.grant_type = "password" // bod.append("grant_type", "password");
-        bod.username = process.env.USERNAME // bod.append("username", process.env.USERNAME);
-        bod.password = process.env.PASSWORD // bod.append("password", process.env.PASSWORD);
+        bod.grant_type = "password"
+        bod.username = creds.username || process.env.USERNAME
+        bod.password = creds.password || process.env.PASSWORD
     }
-    bod.client_id = process.env.CLIENT_ID // bod.append("client_id", process.env.CLIENT_ID);
-    bod.client_secret = process.env.CLIENT_SECRET // bod.append("client_secret", process.env.CLIENT_SECRET);
+    bod.client_id = process.env.CLIENT_ID
+    bod.client_secret = process.env.CLIENT_SECRET
     return fetch(process.env.TOKEN_URL, {
         'method': 'POST',
         'headers': {
@@ -32,46 +34,9 @@ const getToken = (refresh_token) => {
         // return await rs.json()
         .then(response => {
             // console.log('response: ', response)
-            return response.json();
-        }).then(json => {
-            console.log('New token: ', json)
-            return json;
-        }).catch(err => {
-            console.log('Error in getToken: ' + err)
-            return { error: true, ...err };
-        })
-}
-
-const checkToken = async (req, res, isProtected) => {
-    const isServer = !!req
-    const isBrowser = !req
-    let ct
-    if (isBrowser) {
-        console.log('running checkToken in the BROWSER')
-        ct = cookieCutter.get('access_token')
-    } else if (isServer) {
-        console.log('running checkToken in the SERVER')
-        const cookies = new Cookies(req, res)
-        ct = cookies.get('access_token')
-    }
-    if(ct && ct != null && ct != undefined && JSON.parse(ct).exp > Date.now()) {
-        console.log('Token is valid')
-        return JSON.parse(ct)
-    }
-    //check of cookie has expired
-    if (!ct || ct == null || ct == undefined || (ct && JSON.parse(ct).expires > Date.now())) {
-        console.log('Token expired. Refreshing...')
-        if(res){//check if protected page too
-            res.writeHead(301, { Location: '/auth/login?was='+req.url })
-            // res.writeHead(301, { Location: '/auth/login' })
-            res.end()
-        }
-        let refresh_token
-        if(ct && JSON.parse(ct).refresh_token){
-            refresh_token = JSON.parse(ct).refresh_token
-        }
-        return getToken(refresh_token).then(tk => {
-            if (!tk.error) {
+            let tk = response.json();
+            ////
+            if (tk && tk.access_token && tk.expires_in && tk.access_token > 0) {
                 console.log('Token refreshed.')
                 let expiry = new Date(new Date().getTime() + (parseInt(tk.expires_in) * 1000))
                 let tkn = {
@@ -94,6 +59,54 @@ const checkToken = async (req, res, isProtected) => {
                 res.redirect('/auth/login?was='+req.url)
                 return { error: true, ...tk };
             }
+            ////
+            return tk
+        }).then(json => {
+            console.log('New token: ', json)
+            return json;
+        }).catch(err => {
+            console.log('Error in getToken: ' + err)
+            return { error: true, ...err };
+        })
+}
+
+const checkToken = async (req, res, isProtected, creds) => {
+    const isServer = !!req
+    const isBrowser = !req
+    let ct
+    if (isBrowser) {
+        console.log('running checkToken in the BROWSER')
+        ct = cookieCutter.get('access_token')
+    } else if (isServer) {
+        console.log('running checkToken in the SERVER')
+        const cookies = new Cookies(req, res)
+        ct = cookies.get('access_token')
+    }
+    if(ct && ct != null && ct != undefined && JSON.parse(ct).exp > Date.now()) {
+        console.log('Token is valid')
+        return JSON.parse(ct)
+    }
+    //check of cookie has expired
+    if (!ct || ct == null || ct == undefined || (ct && JSON.parse(ct).expires > Date.now())) {
+        console.log('Token expired. Refreshing...')
+        if(res){//check if protected page too
+            res.writeHead(301, { Location: '/auth/login?was='+encodeURIComponent(req.url) })
+            // res.writeHead(301, { Location: '/auth/login' })
+            res.end()
+        }
+        let refresh_token
+        if(ct && JSON.parse(ct).refresh_token){
+            refresh_token = JSON.parse(ct).refresh_token
+        }
+        return getToken(req, res, refresh_token, creds).then(tk => {
+            if (!tk.error) {
+                console.log('Token refreshed.')
+                return tkn;
+            } else {
+                console.log('Error refreshing token: ', tk)
+                res.redirect('/auth/login?was='+encodeURIComponent(req.url))
+                return { error: true, ...tk };
+            }
         })
     } else {
         ct = JSON.parse(ct)
@@ -102,4 +115,28 @@ const checkToken = async (req, res, isProtected) => {
     }
 }
 
-module.exports = { checkToken, getToken }
+const logIn = (req, res, creds, was) => {
+    const isServer = !!req
+    const isBrowser = !req
+    console.log('LogIn: ', creds)
+    return checkToken(req, res, false, creds).then(tk => {
+        console.log('Token: ', tk)
+        if (tk.error) {
+            console.log('Error in LogIn: ', tk)
+            res.redirect('/auth/login?was='+encodeURIComponent(req.url)+'&err='+JSON.stringify(tk))
+            return { error: true, ...tk };
+        } else {
+            console.log('LogIn ok: ', tk)
+            res.writeHead(301, { Location: decodeURIComponent(was) })
+            // return tk;
+        }
+    }).then(tk => {
+        console.log('New token: ', tk)
+        return tk;
+    }).catch(err => {
+        console.log('Error in LogIn: ' + err)
+        return { error: true, ...err };
+    })
+}
+
+module.exports = { checkToken, getToken, logIn }
--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,7 +1,13 @@
+import { set } from 'cookie-cutter'
 import Head from 'next/head'
 import React from 'react'
+import { logIn } from '../../controllers/auth/auth'
 
 const Login = (props) => {
+    const [username, setUsername] = React.useState('')
+    const [password, setPassword] = React.useState('')
+    const [error, setError] = React.useState(props.error)
+    const [loading, setLoading] = React.useState(false)
     return (
         <div>
             <Head>
@@ -15,16 +21,56 @@ const Login = (props) => {
                 </div>
                 <form className="bg-gradient-to-tl from-blue-500 via-green-500 to-green-800 w-full max-w-screen-sm rounded-md p-4 md:p-6 drop-shadow backdrop-filter flex flex-col items-center gap-4 md:gap-9 ">
                     <h1 className="text-center text-3xl font-black text-green-100">Log in</h1>
-                    <div class="flex flex-col gap-0 w-full">
+                    <div className="flex flex-col gap-0 w-full">
+                        {error && error.length > 0 && <p className="text-red-900 bg-red-200 drop-shadow rounded py-1 normal-case text-base px-3">{error}</p>}
+                    </div>
+                    <div className="flex flex-col gap-0 w-full">
                         <label className="text-gray-100">Username</label>
-                        <input type="email" className="text-gray-900 bg-gray-50 border border-gray-300 py-3 px-3 w-full flex items-center leading-none rounded" placeholder="you@geemail.com" />
+                        <input type="email" value={username} onChange={ev=>{
+                            setError('')
+                            setUsername(ev.target.value)
+                        }} className="text-gray-900 bg-gray-50 border border-gray-300 py-3 px-3 w-full flex items-center leading-none rounded" placeholder="you@geemail.com" />
                     </div>
-                    <div class="flex flex-col gap-0 w-full">
+                    <div className="flex flex-col gap-0 w-full">
                         <label className="text-gray-100">Password</label>
-                        <input type="password" className="text-gray-900 bg-gray-50 border border-gray-300 py-3 px-3 w-full flex items-center leading-none rounded" placeholder="*********" />
+                        <input type="password" value={password} onChange={ev=>{
+                            setError('')
+                            setPassword(ev.target.value)
+                        }} className="text-gray-900 bg-gray-50 border border-gray-300 py-3 px-3 w-full flex items-center leading-none rounded" placeholder="*********" />
                     </div>
-                    <div class="flex flex-col gap-4 w-full text-center">
-                        <button className="bg-black hover:text-black hover:bg-gray-300 focus:text-black focus:bg-gray-300 active:text-black active:bg-gray-300 focus:outline-none focus:ring-1 focus:ring-indigo-500 text-white px-4 md:px-8 whitespace-nowrap py-3 rounded text-xl font-semibold" type="submit">Log in</button>
+                    <div className="flex flex-col gap-4 w-full text-center">
+                        <button disabled={loading} className={"hover:text-black hover:bg-white focus:text-black focus:bg-white active:text-black active:bg-white focus:outline-none focus:ring-1 focus:ring-indigo-500 text-white px-4 md:px-8 whitespace-nowrap py-2 rounded text-xl font-semibold border-4 border-black "+(loading ? "bg-gray-700 cursor-not-allowed" : "bg-black")} onClick={ev=>{
+                            setLoading(true)
+                            setError('')
+                            if(
+                                (username && username.length > 0) &&
+                                (password && password.length > 0)
+                            ){
+                                logIn(props.req, props.res, {username, password}, was).then(tkn_rst => {
+                                    if(tkn_rst && tkn_rst.error){
+                                        setError(tkn_rst.error)
+                                    }else{
+                                        setUsername('')
+                                        setPassword('')
+                                        setLoading(false)
+                                        props.res.cookie('token', tkn_rst.token, {
+                                            maxAge: tkn_rst.expires_in * 1000,
+                                            httpOnly: true,
+                                            secure: false,
+                                            signed: true
+                                        })
+                                        props.res.redirect(props.was)
+                                    }
+                                    setLoading(false)
+                                }).catch(err => {
+                                    setLoading(false)
+                                    setError(err)
+                                })
+                            }else{
+                                setLoading(false)
+                                setError('Please fill in all fields')
+                            }
+                        }}>{loading?"Loading...":"Log in"}</button>
                         <div className="flex justify-between items-center w-full px-2">
                             <a href="/" className="text-base text-green-100 hover:underline focus:underline active:underline">Forgot password?</a>
                             <a href="/" className="text-base  text-green-100 hover:underline focus:underline active:underline">Request account</a>
@@ -43,8 +89,18 @@ const Login = (props) => {
 }
 
 Login.getInitialProps = async (ctx) => {
+    const was = ctx.query.was || '/'
+
+    // checkToken(ctx.req, ctx.res, false).then(tkn_rst => {
+    //     if (!tkn_rst || tkn_rst.error) {
+    //         ctx.redirect('/')
+    //     }
+    // })
     return {
-        'ctx': 1
+        'req':ctx.req,
+        'res':ctx.res,
+        'was': was,
+        'error': ''
     }
 }
 
