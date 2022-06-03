5bc19f6e8586c0ce71e8d55b8462377e02c1b2b4
Cynthia Chebet <chebetcynthia8@gmail.com>
2022-06-03 11:53:12 +0300

    dashboard modifications for download & subcounties

---
+++ components/Download.js
@@ -0,0 +1,15 @@
+import { CSVLink } from 'react-csv';
+
+const Download =(props)=>{
+            return (
+
+                <a><CSVLink
+                    headers={props?.csvHeaders}
+                    filename={`${props?.filename}`+'_'+new Date().toLocaleDateString()+'.csv'}
+                    data={props?.data}
+                    ref={props?.csvLink}
+                    target='_blank'>Download</CSVLink></a>
+            
+            )
+}
+export default Download
