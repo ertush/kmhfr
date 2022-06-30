35f98f953f7940f451db82d797c2342b65639763
Reuben Jefwa <mrreubenyt@gmail.com>
2022-06-30 12:01:09 +0300

    task: Fixed CommUnit.initialProps function to made CHU Details dynamic

--- controllers/reject.js
+++ controllers/reject.js
@@ -1,49 +1,45 @@
-import { checkToken, getToken } from "./auth/auth"
+import { checkToken, getToken } from "./auth/auth";
 
-const  approveRejectCHU = async(isApproved, setState,id) => {
+const approveRejectCHU = async (isApproved, setState, id) => {
+  //   let url = `http://api.kmhfltest.health.go.ke/api/chul/units/?is_rejected=true&fields=${cu.id},${cu.code},${cu.name},${cu.status_name},${cu.facility_name},${cu.facility_county},${cu.facility_subcounty},${cu.facility_ward},${cu.date_established}/`;
+  let url = `http://api.kmhfltest.health.go.ke/api/chul/updates/${id}/`;
 
-    let url = `http://api.kmhfltest.health.go.ke/api/chul/units/${id}/`;
-    window.alert(url)
-    const token = await checkToken();
+  if (isApproved) {
+    setState(true);
 
-    if (isApproved){
-        setState(true)
+    return checkToken()
+      .then((token) => {
+        console.log({ token });
         fetch(url, {
-            method: 'PATCH',
-            headers: {
-                "Accept": "application/json",
-                'cache-control': "no-cache",
-                "Content-Type": "application/json;charset=UTF-8", //"multipart/form-data; boundary=---011000010111000001101001",
-                "Authorization": "Basic " + token
-            },
-            body: JSON.stringify({
-                is_approved: true,
-                is_rejected: false,
-            })
-        })
-            .then(res => res.json())
+          method: "GET",
+          headers: {
+            Accept: "application/json",
 
-    } else {
-        
-        setState(false)
-
-    }
-
-}
+            Authorization: "Bearer " + token,
+          },
+          //   body: JSON.stringify({
+          //     is_approved: false,
+          //   }),
+        }).then((res) => console.log(res.json()));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  } else {
+    setState(false);
+  }
+};
 
 const rejectCHU = (e, ctx, state, comment) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    if(state){
-        ctx.is_approved = false;
-    }else {
-        ctx.is_approved = true;
-    }
+  if (state) {
+    ctx.is_approved = false;
+  } else {
+    ctx.is_approved = true;
+  }
 
-    console.log({comment})
-}
+  console.log({ comment });
+};
 
-export {
-    approveRejectCHU,
-    rejectCHU
-}
+export { approveRejectCHU, rejectCHU };
