5bcfc67b7fe370008629bdcdd96b21c3cc879474
FelixKiprotich350 <fkiprotich845@gmail.com>
2023-04-17 11:58:41 +0300

    v3 login playwright tests

---
+++ mflLogin/playwright1/fixtures.js
@@ -0,0 +1,54 @@
+// playwright/fixtures.ts
+import { test as baseTest } from '@playwright/test';
+import fs from 'fs';
+import path from 'path';
+
+export * from '@playwright/test';
+export const test = baseTest.extend<{}, { workerStorageState: string }({
+  // Use the same storage state for all tests in this worker.
+  storageState: ({ workerStorageState }, use) => use(workerStorageState),
+
+  // Authenticate once per worker with a worker-scoped fixture.
+  workerStorageState: [async ({ browser }, use) => {
+    // Use parallelIndex as a unique identifier for each worker.
+    const id = test.info().parallelIndex;
+    const fileName = path.resolve(test.info().project.outputDir, `.auth/${id}.json`);
+
+    if (fs.existsSync(fileName)) {
+      // Reuse existing authentication state if any.
+      await use(fileName);
+      return;
+    }
+
+    // Important: make sure we authenticate in a clean environment by unsetting storage state.
+    const page = await browser.newPage({ storageState: undefined });
+
+    // Acquire a unique account, for example create a new one.
+    // Alternatively, you can have a list of precreated accounts for testing.
+    // Make sure that accounts are unique, so that multiple team members
+    // can run tests at the same time without interference.
+    const account = await acquireAccount(id);
+
+    // Perform authentication steps. Replace these actions with your own.
+    await page.goto("http://admin.kmhfltest.health.go.ke");
+  await page.getByRole("link", { name: "Log in" }).click();
+  await page.getByPlaceholder("you@geemail.com").click();
+  await page.getByPlaceholder("you@geemail.com").fill("test@mflcountyuser.com");
+  await page.getByPlaceholder("*********").click();
+  await page.getByPlaceholder("*********").fill("county@12345");
+  await page.getByRole("button", { name: "Log in" }).click();
+    // Wait until the page receives the cookies.
+    //
+    // Sometimes login flow sets cookies in the process of several redirects.
+    // Wait for the final URL to ensure that the cookies are actually set.
+    await page.waitForURL('http://admin.kmhfltest.health.go.ke/dashboard');
+    // Alternatively, you can wait until the page reaches a state where all cookies are set.
+    await expect(page.getByRole('button', { name: 'profile' })).toBeVisible();
+
+    // End of authentication steps.
+
+    await page.context().storageState({ path: fileName });
+    await page.close();
+    await use(fileName);
+  }, { scope: 'worker' }],
+});
