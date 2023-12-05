233b8d4d4e0164f3fabaf19964a3dfdb2b7b2293
Cynthia Chebet <chebetcynthia8@gmail.com>
2023-12-05 19:26:46 +0300

    fix search and table display

--- components/Forms/formComponents/EditListWithCount.js
+++ components/Forms/formComponents/EditListWithCount.js
@@ -10,6 +10,7 @@ import {
 } from '@heroicons/react/solid';
 import { useAlert } from 'react-alert'
 import { useLocalStorageState } from '../hooks/formHook';
+import { number } from 'zod';
 
 
 function EditListWithCount(
@@ -47,10 +48,49 @@ function EditListWithCount(
     const [deletedItems, setDeletedItems] = useState([])
     const [itemOptions, setItemOptions] = useState([])
     const [specialities, setSpecialities] = useState([])
-    const [query, setQuery] = useState('')
-    const [categoryOptions, setCategoryItems] = useState(categoryItems)
+    const [query, setQuery] = useState('') 
     const [selectedRows, setSelectedRows] = useState([]);
 
+    const [categoryOptions, setCategoryItems] = useState( (test)=> {
+
+         
+        let newarray=[];
+        categoryItems.forEach(element => {
+            let customitem={value:element.value, label:element.label, catcount:0}
+            newarray.push(customitem);
+        });
+        return newarray;
+  });
+    // });
+    function CountCategoryTotalSpecialities(specialityid,newvalue){
+        let total=0;
+        let _categoryid="";
+        selectedRows.forEach(element => {
+            if(element.rowid==specialityid){
+                _categoryid=element.categoryid;
+               
+            }
+        }); 
+        categoryOptions.forEach(item => {
+            if(item.value==_categoryid){
+                selectedRows.filter(k=>k.categoryid==_categoryid).forEach(element => {
+                    if(element.rowid==specialityid){
+                        element.count=newvalue;
+                    }
+                    total=total+ parseInt( element.count);
+                });
+             }
+        });
+        // console.log(total,_categoryid)
+        if(categoryOptions.some(item=>item.value==_categoryid)){
+            setCategoryItems(prevArray => 
+                prevArray.map(item => 
+                  item.value === _categoryid ? { ...item, catcount: total } : item
+                )
+              );
+        }
+    }
+    // console.log(categoryOptions)
 
     const [selectedItems, setSelectedItems] = useState((initialSelectedItems ? (() => {
         const result = []
@@ -62,7 +102,7 @@ function EditListWithCount(
 
             })
         }
-
+``
         return result
 
     })() : []))
@@ -202,39 +242,63 @@ function EditListWithCount(
     }, [isFormSubmit])
 
     const filterSpecialities = (ctg) => {
-        const filteredOptions = options.filter((option) => option.category_name === ctg );
+        const filteredOptions = options.filter((option) => option.category_id=== ctg );
         setSpecialities(filteredOptions)
 
     }
 
-    const handleCheckboxChange = (id) => {
+    const handleCheckboxChange = (id, name,category) => {
         setSelectedRows((prevSelectedRows) => {
-          if (prevSelectedRows.includes(id)) {
-            return prevSelectedRows.filter((rowId) => rowId !== id);
+          if (prevSelectedRows.filter((row) => row.rowid == id).length>0) {
+            return prevSelectedRows.filter((row) => row.rowid !== id);
           } else {
-            return [...prevSelectedRows, id];
+              let customitem={rowid:id, sname:name, count:0,categoryid:category}
+            return [...prevSelectedRows, customitem];
           }
         });
-      };
+      }; 
+    //   console.log(specialities)
+      
+    const handleInputChange = (rowvalue, targetvalue) => {
+          // Update the selected rows values
+          if(selectedRows.some(item=>item.rowid==rowvalue)){
+              setSelectedRows(prevArray => 
+                  prevArray.map(item => 
+                    item.rowid === rowvalue ? { ...item, count: targetvalue } : item
+                  )
+                );
+                
+          }
+          CountCategoryTotalSpecialities(rowvalue,targetvalue)
+      };  
+  
+    //   console.log(specialities)
+  
 
-    const onSearch = ((event, searchtype)=>{
+    const onSearch = ((event, issearchcategory,issearchspeciality)=>{
 
         const _query=event.target.value;
+        console.log(_query)
         setQuery(_query);
         if(_query.length > 3){
-            if(searchtype === 'category_search'){
+            if(issearchcategory){
                 let subset = categoryItems.filter(function (el) {
                     return el.label.toLowerCase().includes(_query.toLowerCase());
                 }
                 );
                 setCategoryItems(subset);
-            }else if(searchtype === 'speciality_search'){
-                let specialities = specialities.filter((e)=>e.name.toLowerCase().includes(_query.toLowerCase()))
-                setSpecialities(specialities);
+            }else if(issearchspeciality ){
+                let _specialities = specialities.filter((e)=>e.name.toLowerCase().includes(_query.toLowerCase()))
+                setSpecialities(_specialities);
             }
         }
         else{
-             
+            if(issearchspeciality){
+                filterSpecialities(specialities[0].category_id)
+            }
+            setCategoryItems(categoryItems);
+            setSpecialities(specialities);
+            //  return categoryItems, specialities;
         }
     });
     // console.log({ options, categoryItems })
@@ -639,14 +703,14 @@ function EditListWithCount(
 
                     <div className='w-full grid grid-cols-12 gap-4'>
                          <div className="col-span-5" >
-                            <input type="text" onChange={(e)=>onSearch(e,'category_search')} className="col-span-12 border border-blue-600 p-2 placeholder-gray-500  focus:shadow-none focus:bg-white focus:border-black outline-none" placeholder="Search" />
+                            <input type="text" onChange={(e)=>onSearch(e,true,false)} className="col-span-12 border border-blue-600 p-2 placeholder-gray-500  focus:shadow-none focus:bg-white focus:border-black outline-none" placeholder="Search" />
                             <ul>
                                 {categoryOptions.map(({label, value}) => (
                                     <>
 
                                         <div key={value} className='card bg-blue-50 shadow-md p-2'>
 
-                                            <li className="flex items-center justify-start space-x-2  p-1 px-2  hover:bg-red" onClick={()=>filterSpecialities(label)} key ={value}>{label}</li>
+                                            <li className="flex items-center justify-start space-x-2  p-1 px-2  hover:bg-red" onClick={()=>filterSpecialities(value)} key ={value}>{label}</li>
                                             <hr></hr>
                                         </div>
                                     </>
@@ -654,7 +718,7 @@ function EditListWithCount(
                             </ul>
                          </div>
                          <div className="col-span-7" >
-                                <input type="text" onChange={(e)=>onSearch(e,'speciality_search')} className="col-span-12 border border-blue-600 p-2 placeholder-gray-500  focus:shadow-none focus:bg-white focus:border-black outline-none" placeholder="Search" />
+                                <input type="text" onChange={(e)=>onSearch(e,false,true)} className="col-span-12 border border-blue-600 p-2 placeholder-gray-500  focus:shadow-none focus:bg-white focus:border-black outline-none" placeholder="Search" />
                                 {/* {specialities.length === 0 && <p className="text-center">No specialities found</p>} */}
                                 {/* <ul>
                                 {specialities.map(({id, name})=>(
@@ -697,24 +761,32 @@ function EditListWithCount(
                                             <input
                                                 type="checkbox"
                                                 className="p-1 w-5 h-5"
-                                                checked={selectedRows.includes(row.id)}
-                                                onChange={() => handleCheckboxChange(row.id)}
+                                                checked={selectedRows.some(item=>item.rowid.includes(row.id))}
+                                                onChange={() => handleCheckboxChange(row.id, row.name,row.category)}
                                             /> Yes
                                             </td>
                                             <td className="border px-1 py-1">
                                             <input
                                                 type="number"
-                                                className="p-1" min={0}
-                                                disabled={!selectedRows.includes(row.id)}
-                                            />
+                                                className="p-1" 
+                                                min={0}
+                                                defaultValue={selectedRows.filter(k=>k.rowid==row.id).length>0?Number(selectedRows.filter(k=>k.rowid==row.id)[0].count):0}
+                                                onChange={(e) => handleInputChange(row.id, e.target.value)}
+                                                disabled={!selectedRows.some(item=>item.rowid.includes(row.id))}                                            />
                                             </td>
                                         </tr>
                                         ))}
                                     </tbody>
                                 </table>
 
-                                {/* summary table */}
-                                {/* <table className="table-auto w-full">
+                                
+
+                         </div>
+
+                         {/* summary table */}
+                         <div className="col-span-12" >
+
+                         <table className="table-auto w-full">
                                     <thead>
                                         <tr>
                                         <th className="border px-1 py-1">Name</th>
@@ -723,8 +795,8 @@ function EditListWithCount(
                                         </tr>
                                     </thead>
                                     <tbody className='bg-blue-50 shadow-md'>
-                                        {specialities.length === 0 && <tr><td colSpan={3} className="text-center">No specialities found</td></tr>}
-                                        {specialities.map((row) => (
+                                        {selectedRows.length === 0 && <tr><td colSpan={3} className="text-center">No specialities found</td></tr>}
+                                        {selectedRows.map((row) => (
                                         // <tr key={row.id}>
                                         //     <td className="border px-1 py-1">
                                         //     <label  className="w-full p-2" >{row.name}</label>
@@ -746,14 +818,13 @@ function EditListWithCount(
                                         //     </td>
                                         // </tr>
                                         <tr>
-                                            <td className="border px-1 py-1"></td>
-                                            <td className="border px-1 py-1"></td>
-                                            <td className="border px-1 py-1"></td>
+                                            <td className="border px-1 py-1">{row.sname}</td>
+                                            <td className="border px-1 py-1">Yes</td>
+                                            <td className="border px-1 py-1">{Number(row.count)}</td>
                                         </tr>
                                         ))}
                                     </tbody>
-                                </table> */}
-
+                         </table>
                          </div>
                     </div>
 
