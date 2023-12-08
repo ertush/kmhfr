1dba5f590ead0262c45c21cbc3806165607da316
Cynthia Chebet <chebetcynthia8@gmail.com>
2023-12-08 11:31:31 +0300

    services restructure

--- components/Forms/formComponents/EditListItem.js
+++ components/Forms/formComponents/EditListItem.js
@@ -73,17 +73,19 @@ function  EditListItem({
 //   const [isRemoveItem, setIsRemoveItem] = useState(false)
   const [itemOptions, setItemOptions] = useState([])
   const [deletedItems, setDeletedItems] = useState([])
+  const [allServices, setallServices] = useState([])
   const [selectedItems, setSelectedItems] = useState((initialSelectedItems ? (() => {
   const result = []
 
-  initialSelectedItems.map(({ subCategories, value, facility_service_id: item_id }) => {
+  initialSelectedItems.map((service) => {
 
-    result.push({ name: subCategories[0], id: value[0], item_id })
+    result.push({ sname: service.service_name, rowid: service.service_id, category_id:service.category_id,category_name:service.category_name })
 
   })
     return result
 
   })() : []))
+  console.log({categoryItems, options})
 
   const editService = servicesData?.map(({service_name:name,  service_id, id}) => ({id, service_id, name}));
 
@@ -97,6 +99,54 @@ function  EditListItem({
   // Refs
 
   const itemRef = useRef(null);
+
+  const [categoryOptions, setCategoryItems] = useState(()=> {
+
+    let newarray=[];
+    categoryItems.forEach(element => {
+        let customitem ={}
+        customitem={value:element.value, label:element.label}
+        newarray.push(customitem);
+    });
+    return newarray;
+  });
+
+  const filterSpecialities = (ctg) => {
+    const filteredOptions = options.filter((option) => option.category === ctg );
+    setallServices(filteredOptions)
+}
+
+  const handleCheckboxChange = (id, name,category, category_name) => {
+      setSelectedItems((prevSelectedRows) => { 
+      if (prevSelectedRows.filter((row) => row.rowid == id).length>0) {
+        return prevSelectedRows.filter((row) => row.rowid !== id);
+      } else {
+        let customitem = {}
+        customitem={rowid:id, sname:name, category_id:category,category_name:category_name}
+        return [...prevSelectedRows, customitem];
+      }
+      });  
+  };
+
+  const onSearch = ((event, issearchcategory,issearchservice)=>{
+
+    const _query=event.target.value;
+    if(_query.length > 3){
+        if(issearchcategory){
+            let subset = categoryItems.filter((e)=>e.label.toLowerCase().includes(_query.toLowerCase()))
+            setCategoryItems(subset);
+        }else if(issearchservice ){
+            let _specialities = allServices.filter((e)=>e.name.toLowerCase().includes(_query.toLowerCase()))
+            setallServices(_specialities);
+        }
+    }
+    else{
+        if(issearchservice){
+            filterSpecialities(allServices[0].category)
+        }
+        setCategoryItems(categoryItems);
+    }
+}); 
   
 
   useEffect(() => {
@@ -210,9 +260,154 @@ function  EditListItem({
         className="flex flex-col w-full items-start justify-start gap-3"
 
       >
+        
+        <div className='w-full grid grid-cols-12 gap-4'>
+              <div className="col-span-5" >
+                <h4 className="text-lg uppercase mt-4 pb-2 border-b border-blue-600 w-full mb-4 font-semibold text-blue-900">Categories</h4>
+                <input type="text" onChange={(e)=>onSearch(e,true,false)} className="col-span-12 border border-blue-600 p-2 placeholder-gray-500  focus:shadow-none focus:bg-white focus:border-black outline-none w-full" placeholder="Search" />
+                <br/>
+                <ul className='max-h-96 overflow-auto'>
+                    {categoryOptions.map(({label, value}) => (
+                        <>
+
+                            <div key={value} 
+                            className={`card bg-blue-50 shadow-md p-2 hover:bg-blue-500`}
+
+                            >
+                                <li 
+                                className="flex items-center justify-start space-x-2 p-1 px-2"
+                                onClick={()=>{
+                                    filterSpecialities(value)
+                                }} 
+                                    key ={value}>{label}</li>
+                                <hr></hr>
+                            </div>
+                        </>
+                    ))}
+                </ul>
+              </div>
+              <div className="col-span-7" >
+                    <h4 className="text-lg uppercase mt-4 pb-2 border-b border-blue-600 w-full mb-4 font-semibold text-blue-900">Services</h4>
+                    <input type="text" onChange={(e)=>onSearch(e,false,true)} className="col-span-12 border border-blue-600 p-2 placeholder-gray-500  focus:shadow-none focus:bg-white focus:border-black outline-none w-full" placeholder="Search" />
+                    <br/>
+                    <div className='max-h-96 overflow-auto'>
+
+                        <table className="table-auto w-full">
+                            <thead>
+                                <tr>
+                            
+                                </tr>
+                            </thead>
+                            <tbody className='bg-blue-50 shadow-md'>
+                                {allServices.length === 0 && <tr><td colSpan={3} className="text-center">No services found</td></tr>}
+                                
+                                {allServices.map((row) => (
+                                    
+                                <tr key={row.id}> 
+                                    <td className="border px-1 py-1">
+                                    <label  className="w-full p-2" >{row.name}</label>
+                                    </td>
+                                    <td className="border px-1 py-1">
+                                    <input
+                                        type="checkbox"
+                                        className="p-1 w-5 h-5"
+                                        checked={selectedItems.some(item=>item.rowid.includes(row.id))}
+                                        onChange={(e) => handleCheckboxChange(
+                                            row.id,
+                                            row.name,
+                                            row.category, 
+                                            row.category_name,
+                                        )}
+                                    /> Yes
+                                    </td>
+                                    {/* <td className="border px-1 py-1">
+                                    <Field
+                                        as="input"
+                                        type="number"
+                                        className="p-1" 
+                                        min={0}
+                                        name={row.id}
+                                        // defaultValue={selectedRows.filter(k=>k.rowid==row.id).length>0?Number(selectedRows.filter(k=>k.rowid==row.id)[0].count):0}
+                                        // onChange={(e) => handleInputChange(row.id, e.target.value)}
+                                        onChange ={(e)=>{
+                                            handleChange(e)
+                                            let cid=row.id
+                                            handleInputChange(cid, e.target.value)
+                                        
+                                        }}
+                                        hidden={!selectedRows.some(item=>item["count"])}
+                                        disabled={!selectedRows.some(item=>item.rowid.includes(row.id))}  
+                                    />
+                                    </td> */}
+                                </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+
+                    
+
+              </div>
+
+              <div className="col-span-12 max-h-96 overflow-auto" >
+
+                <table className="table-auto w-full">
+                            <thead>
+                                <tr>
+                                
+                                    <th className="border px-1 py-1" key={'services'}>Services</th>
+                                    <th className="border px-1 py-1" key={'services'}>Present</th>
+                              
+                                </tr>
+                            </thead>
+                            <tbody className='bg-blue-50 shadow-md'>
+                                {selectedItems.length === 0 && <tr><td colSpan={3} className="text-center">No services found</td></tr>}
+                                {selectedItems.map((row) => (
+                                  <tr>
+                                      <td className="border px-1 py-1">{row.sname}</td>
+                                      <td className="border px-1 py-1">Yes</td>
+                                  </tr>
+                                ))}
+
+                            </tbody>
+                </table>
+              </div>
+        </div>
+
+        {/* Save btn */}
+
+        {
+          savedItems.length > 0 && item !== null &&
+
+          <div className=" w-full flex justify-end h-auto mr-3">
+            <button type='submit' className='p-2 text-white bg-blue-600  font-semibold'>save & finish</button>
+          </div>
+        }
+
+        {
+          item === null &&
+
+          <div className='flex justify-between items-center w-full mt-4' >
+              <button onClick={handleItemPrevious} className='flex items-center justify-start space-x-2 p-1 border border-blue-900  px-2'>
+                <ChevronDoubleLeftIcon className='w-4 h-4 text-blue-900' />
+                <span className='text-medium font-semibold text-blue-900 '>
+                  {previousItemCategory}
+                </span>
+              </button>
+              <button
+                type='submit'
+                className='flex items-center justify-start space-x-2 bg-blue-700  p-1 px-2'>
+                <span className='text-medium font-semibold text-white'>
+                  {nextItemCategory}
+                </span>
+                <ChevronDoubleRightIcon className='w-4 h-4 text-white' />
+              </button>
+           
+          </div>
+        }
+
         {/* Item List Dropdown */}
-        <div className='w-full flex flex-col items-start bg-blue-50 shadow-md p-4 justify-start gap-1 mb-3'>
-          {/* Iten Category Dropdown */}
+        {/* <div className='w-full flex flex-col items-start bg-blue-50 shadow-md p-4 justify-start gap-1 mb-3'>
           {
           itemsCategoryName !== 'CHU Services' &&
           <>
@@ -298,14 +493,12 @@ function  EditListItem({
           </>
           }
           
-          {/* Item Dropdown */}
           <label
             htmlFor='item_drop_down_edit_list'
             className='capitalize mt-4 text-md font-semibold leading-tight tracking-tight'>
              {itemsCategoryName}
           </label>
           <div className="flex items-start gap-2  w-full h-auto">
-            {/* {console.log({itemOptions})} */}
             <Select
               options={itemsCategoryName !== 'CHU Services' ? itemOptions : null}
               formatGroupLabel={formatGroupLabel}
@@ -333,7 +526,6 @@ function  EditListItem({
              
             />
 
-            {/* Add Item Button */}
             <button name="add_item_btn" className="bg-blue-700  p-2 flex items-center justify-evenly gap-2"
               onClick={e => {
                 e.preventDefault()
@@ -347,12 +539,12 @@ function  EditListItem({
               <PlusIcon className='w-4 h-4 text-white' />
             </button>
           </div>  
-        </div>
+        </div> */}
 
 
         {/* Item Selected Table */}
        
-        <Table className="card bg-blue-50 border-b shadow-md" >
+        {/* <Table className="card bg-blue-50 border-b shadow-md" >
           <TableBody >
             <TableRow >
               <TableCell className='bg-transparent text-blue-700 border-b border-blue-600'>
@@ -375,7 +567,6 @@ function  EditListItem({
             </TableRow>
 
             <>
-              {/* {console.log({items})} */}
               {
                 typeof items === 'object' && 
                 items.map(({ name, id }, __id) => (
@@ -383,18 +574,7 @@ function  EditListItem({
                     key={__id}
                     className='border-t border-blue-600'
                   >
-                    {/* {
-                  items !== null &&
-                  <>
-                    <li className="w-full m-3 bg-yellow-100 flex flex-row gap-2 my-2 p-3 border border-yellow-300 text-yellow-900 text-base">
-                      <p>
-                        {item?.name || item?.official_name} has not listed
-                        the {'item'} it offers. Add some below.
-                      </p>
-                    </li>
-                    <br />
-                  </>
-                  } */}
+                   
                     <TableCell className='px-3'>{name}</TableCell>
 
                     <TableCell>
@@ -440,39 +620,7 @@ function  EditListItem({
              }
             </>
           </TableBody>
-        </Table>
-
-        {/* Save btn */}
-
-        {
-          savedItems.length > 0 && item !== null &&
-
-          <div className=" w-full flex justify-end h-auto mr-3">
-            <button type='submit' className='p-2 text-white bg-blue-600  font-semibold'>save & finish</button>
-          </div>
-        }
-
-        {
-          item === null &&
-
-          <div className='flex justify-between items-center w-full mt-4' >
-              <button onClick={handleItemPrevious} className='flex items-center justify-start space-x-2 p-1 border border-blue-900  px-2'>
-                <ChevronDoubleLeftIcon className='w-4 h-4 text-blue-900' />
-                <span className='text-medium font-semibold text-blue-900 '>
-                  {previousItemCategory}
-                </span>
-              </button>
-              <button
-                type='submit'
-                className='flex items-center justify-start space-x-2 bg-blue-700  p-1 px-2'>
-                <span className='text-medium font-semibold text-white'>
-                  {nextItemCategory}
-                </span>
-                <ChevronDoubleRightIcon className='w-4 h-4 text-white' />
-              </button>
-           
-          </div>
-        }
+        </Table> */}
 
       </Form>
     </Formik>
