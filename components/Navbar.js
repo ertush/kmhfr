4deb059d5506bda4bdb544e81e1475622ca147e2
Bett <benzerbett@gmail.com>
2024-02-22 16:01:27 +0300

    Update access groups for various pages in navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -33,19 +33,19 @@ function NavBar({ session, loggedIn, user, searchQ = '' }) {
     const pathname = router.pathname;
     const userCtx = React.useContext(UserContext)
     const groupID = userCtx?.groups[0]?.id
-    const [pages, setPages] = React.useState([
+    const pages = [
         { name: 'Home', link: '/', protected: false },
-        { name: 'Dashboard', link: '/dashboard', protected: true, allowedGroups: [7,2,3,4] },
+        { name: 'Dashboard', link: '/dashboard', protected: true, allowedGroups: [1,2,3,4,5,6,7,10,11,12] },
         { name: 'About', link: '/about', protected: false },
         { name: 'Facilities', link: '/public/facilities', protected: false },
-        { name: 'Facilities', link: '/facilities', protected: true, allowedGroups: [7,2,3,4] },
+        { name: 'Facilities', link: '/facilities', protected: true, allowedGroups: [1,2,3,4,5,6,7,10,11,12] },
         { name: 'Community Units', link: '/public/community-units' },
-        { name: 'Community Units', link: '/community-units', protected: true, allowedGroups: [7,2,3,4] },
-        { name: 'Users', link: '/users', protected: true, allowedGroups: [7, 2, 3] },
-        { name: 'System setup', link: '/system_setup', protected: true, allowedGroups: [7] },
-        { name: 'Reports', link: '/reports', protected: true, allowedGroups: [7,2,3,4] },
+        { name: 'Community Units', link: '/community-units', protected: true, allowedGroups: [1,2,3,4,5,6,7,10,11,12] },
+        { name: 'Users', link: '/users', protected: true, allowedGroups: [1,3,5,10] },
+        { name: 'System setup', link: '/system_setup', protected: true, allowedGroups: [1,3,5,10] },
+        { name: 'Reports', link: '/reports', protected: true, allowedGroups: [1,2,3,4,5,6,7,10,11,12] },
         { name: 'Contact us', link: '/about#contacts', protected: false }
-      ])
+      ]
 
     const handleOpenNavMenu = (event) => {
         setAnchorElNav(event.currentTarget);
@@ -65,10 +65,10 @@ function NavBar({ session, loggedIn, user, searchQ = '' }) {
     React.useEffect(() => {
         let mounted = true;
         if (mounted) {
-            console.log('NavBar: ', user)
+            // setPages(pgs)
         }
         return () => { mounted = false }
-    }, [groupID, loggedIn, user])
+    }, [groupID, loggedIn, user, pages])
 
     return (
         <AppBar position="fixed" color="default" variant="outlined" elevation={0}>
@@ -111,7 +111,6 @@ function NavBar({ session, loggedIn, user, searchQ = '' }) {
                         </Box>
                         {/* desktop */}
                         <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, flexDirection: 'row', gap: { md: 3, lg: 5 } }}>
-
                             {pages.map((page) => {
                                 const allowed = (
                                     (!loggedIn && !page.protected) ||
@@ -123,21 +122,22 @@ function NavBar({ session, loggedIn, user, searchQ = '' }) {
                                     <Link key={page.name} href={page.link}><a style={{ textDecoration: 'none', color: (active ? '#333' : '#1651B6'), borderBottom: `2px solid ${(active ? '#333' : 'transparent')}` }}>{page.name}</a></Link>
                                 )
                             })}
-
                         </Box>
                     </Box>
-                    {/* {!['/'].includes(pathname) && <Box sx={{ display: 'flex', flexGrow: 1 }}>
-                        <Box sx={{ display: { xs: 'flex', lg: 'none' }, alignItems: 'center' }}>
-                            {/-* mobile view *-/}
-                        </Box>
-                            {/-* desktop view *-/}
-                            <Box variant="outlined" sx={{ display: { md: 'none', lg: 'flex' }, alignItems: 'center', px: 2 }}>
-                                <TextField variant="outlined" color="primary" id="search" label="Search" name='q' size="small" sx={{ display: { xs: 'none', md: 'flex' }, width: 'auto' }} value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
-                                <Button variant="outlined" size='small' sx={{ display: { xs: 'none', md: 'flex' }, p: 1 }} onClick={ev=>{
-                                    router.push('/search?q='+searchQuery)
-                                }}><SearchRounded m={4} /></Button>
+                    {/* 
+                        {!['/'].includes(pathname) && <Box sx={{ display: 'flex', flexGrow: 1 }}>
+                            <Box sx={{ display: { xs: 'flex', lg: 'none' }, alignItems: 'center' }}>
+                                {/-* mobile view *-/}
                             </Box>
-                    </Box>} */}
+                                {/-* desktop view *-/}
+                                <Box variant="outlined" sx={{ display: { md: 'none', lg: 'flex' }, alignItems: 'center', px: 2 }}>
+                                    <TextField variant="outlined" color="primary" id="search" label="Search" name='q' size="small" sx={{ display: { xs: 'none', md: 'flex' }, width: 'auto' }} value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
+                                    <Button variant="outlined" size='small' sx={{ display: { xs: 'none', md: 'flex' }, p: 1 }} onClick={ev=>{
+                                        router.push('/search?q='+searchQuery)
+                                    }}><SearchRounded m={4} /></Button>
+                                </Box>
+                        </Box>} 
+                    */}
 
                     {(loggedIn && user) ? (
                         <Box sx={{ flexGrow: 0, display: 'flex', alignItems: 'center', gap: { xs: 0, md: 1 }, border: '1px solid #ccc', color: 'black', py: 1, px: 1, borderRadius: '8px' }}>
