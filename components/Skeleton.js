dd39b5e9082f79f51d967ed50f7f7a2ca9d75334
eric <erotush77@gmail.com>
2024-06-24 12:19:21 +0300

    Fixed the Skeleton Loading indicator

--- components/Skeleton.js
+++
@@ -1,9 +0,0 @@
-export default function Skeleton({w, h, bg}) {
-    return (
-      <div className='max-w-min h-auto animate-pulse'>
-         <div className={`w-[${w ?? '100px'}] h-[${h ?? '24px'}] ${bg} rounded-full`}></div>
-      </div>
-     
-    )
-  }
-  
