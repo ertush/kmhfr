4da9d13be2694a56e162349ba3c1e42475c89a2d
eric <erotush77@gmail.com>
2024-10-24 18:45:36 +0300

    Modified next.config.js to add Cache-Control headers for all pages

--- next.config.js
+++ next.config.js
@@ -2,18 +2,18 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
-const cspHeader = `
-    default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline';
-    style-src 'self' 'unsafe-inline';
-    img-src 'self' blob: data:;
-    font-src 'self';
-    object-src 'none';
-    base-uri 'self';
-    form-action 'self';
-    frame-ancestors 'none';
-    upgrade-insecure-requests;
-`
+// const cspHeader = `
+//     default-src 'self';
+//     script-src 'self' 'unsafe-eval' 'unsafe-inline';
+//     style-src 'self' 'unsafe-inline';
+//     img-src 'self' blob: data:;
+//     font-src 'self';
+//     object-src 'none';
+//     base-uri 'self';
+//     form-action 'self';
+//     frame-ancestors 'none';
+//     upgrade-insecure-requests;
+// `
 
 /** @type {import('next').NextConfig} */
 
@@ -23,19 +23,19 @@ const nextConfig = {
     //   largePageDataBytes: 168960 // 165kB; Default is 128kB
     // }
 
-    // async headers() {
-    //   return [
-    //     {
-    //       source: '/(.*)',
-    //       headers: [
-    //         {
-    //           key: 'Content-Security-Policy',
-    //           value: cspHeader.replace(/\n/g, ''),
-    //         },
-    //       ],
-    //     },
-    //   ]
-    // }
+    async headers() {
+      return [
+        {
+          source: '/(.*)',
+          headers: [
+            {
+              key: 'Cache-Control',
+              value: 'no-cache, no-store, max-age=0'
+            },
+          ],
+        },
+      ]
+    }
 
  
 }   
